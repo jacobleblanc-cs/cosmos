674db2bc5e23a44399c56f662f784f903584b6b4
Jacob LeBlanc <67487789+jacobleblanc-cs@users.noreply.github.com>
2025-04-07 16:07:48 -0400

    Update installNix.sh

--- files/scripts/installNix.sh
+++ files/scripts/installNix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -oue pipefail
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --determinate --no-confirm -- --no-start-daemon
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --determinate --no-confirm --no-start-daemon
