1e419194c7865e79abb0b199d12fb1237c8996ff
Jacob LeBlanc <67487789+jacobleblanc-cs@users.noreply.github.com>
2025-04-07 20:11:18 -0400

    Testing --init none option, as --no-start-daemon failed to fully install

--- files/scripts/installNix.sh
+++ files/scripts/installNix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -oue pipefail
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --determinate --no-confirm
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --determinate --no-confirm --init none
